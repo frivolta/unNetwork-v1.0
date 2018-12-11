2f613cb5ad2f248cc17019ef8a4aa027f39c3709
frivolta <rivoltafilippo@gmail.com>
2018-12-11 01:05:50 +0100

    Added auth, init validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const jwt = require('jsonwebtoken');
 const keys = require ('../../config/keys');
 const router = express.Router();
+const passport = require('passport');
 
 // Load User model
 const User = require('../../models/User');
@@ -86,5 +87,17 @@ router.post('/login', (req,res)=>{
                 })
         })
 })
+
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res)=>{
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    })
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require ('./routes/api/users');
 const profile = require ('./routes/api/profile');
@@ -24,6 +25,14 @@ const db = require ('./config/keys').mongoURI;
 mongoose.connect(db)
     .then(()=>console.log('MongoDB Connected'))
     .catch((err)=>console.log(err));
+
+/**
+ * PASSPORT
+ */
+//Passport middleware
+app.use(passport.initialize());
+//passport config
+require('./config/passport')(passport);
 
 /**
  * ROUTES
