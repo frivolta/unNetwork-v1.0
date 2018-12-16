9f9fcd5c0b318a1882b3467f8535945fe430c0d5
frivolta <rivoltafilippo@gmail.com>
2018-12-16 16:07:05 +0100

    Adding statefull register with error handling, before redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from './Components/Layout/Navbar';
 import Footer from './Components/Layout/Footer';
 import Landing from './Components/Layout/Landing';
+import Register from './Components/Auth/Register';
+import Login from './Components/Auth/Login';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing}/>
+          <div className="container">
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/login" component={Login}/>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
