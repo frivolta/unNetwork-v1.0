b6c05ff2f023df5fb1077aade8fd96dd42137d85
Filippo Rivolta <rivoltafilippo@gmail.com>
2018-12-20 12:38:08 +0100

    Profile header

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -11,19 +11,44 @@ import Spinner from '../Common/Spinner';
 
 
 class Profile extends Component {
-  componentDidMount(){
-    if(this.props.match.params.handle){
+  componentDidMount() {
+    if (this.props.match.params.handle) {
       this.props.getProfileByHandle(this.props.match.params.handle);
     }
   }
 
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+    if (profile === null || loading) {
+      profileContent = <Spinner />
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back to Profiles
+              </Link>
+              <div className="col-md-6" />
+            </div>
+          </div>
+          <ProfileHeader profile={profile}/>
+          <ProfileAbout />
+          <ProfileCreds />
+          <ProfileGithub />
+        </div>
+      )
+    }
     return (
-      <div>
-        <ProfileHeader/>
-        <ProfileAbout/>
-        <ProfileCreds/>
-        <ProfileGithub/>
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              {profileContent}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
