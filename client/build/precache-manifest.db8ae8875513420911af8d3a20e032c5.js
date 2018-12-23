470319d0fcc7679dcedee9a6282009be9e19f32a
frivolta <rivoltafilippo@gmail.com>
2018-12-23 21:20:55 +0100

    Prod ready

---
+++ client/build/precache-manifest.db8ae8875513420911af8d3a20e032c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "669e5c78314d4ff40f14",
+    "url": "/static/js/main.669e5c78.chunk.js"
+  },
+  {
+    "revision": "d5278b8e46e39fc0f515",
+    "url": "/static/js/1.d5278b8e.chunk.js"
+  },
+  {
+    "revision": "669e5c78314d4ff40f14",
+    "url": "/static/css/main.0e7112ab.chunk.css"
+  },
+  {
+    "revision": "df900d5d1ae8fb5bb886bb3416d829b0",
+    "url": "/index.html"
+  }
+];
